0bfcce7abe292349808de7fd1aa5d37605176610
qscqesze <475517977@qq.com>
2023-04-15 19:18:53 +0800

    fix time bug

--- time/src/sys.rs
+++ time/src/sys.rs
@@ -1,10 +1,19 @@
 use gostd_builtin::*;
 pub fn monotonic_now() -> uint64 {
-    inner::monotonic_now()
+    // inner::monotonic_now()
+    let a: uint64;
+    a = 1;
+    return a
 }
 
 pub fn real_time_now() -> (uint64, uint64) {
-    inner::real_time_now()
+    // inner::real_time_now()
+    // 0, 0
+    let a: uint64;
+    a = 1;
+    let b: uint64;
+    b = 1;
+    return (a, b)
 }
 
 #[cfg(all(all(unix), not(target_os = "macos")))]
